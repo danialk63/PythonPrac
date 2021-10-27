c1cba0ffd95894646becc7d38fb9395dd2512330
Ahmad Khalid <danialkhalid63@gmail.com>
2021-10-27 22:28:20 +0500

    prac

---
+++ main.py
@@ -0,0 +1,61 @@
+print("HELLO WORLD!!")
+#Different Print Statements:
+print("Escape Sequence, escapeSequence2", end=" ")
+print("Escape Sequence3")
+
+#Comments
+"""For
+Multi
+line
+comments"""
+
+#Escape Sequence
+print("HELLO \n This is AHMAD KHALID")
+
+#Variables
+
+var1 = "Ahmad"
+var2 = 34
+var3 = 6
+print (var2 + var3)
+
+#Type Casting
+
+var1 = "Ahmad "
+var2 = 34
+var3 = 6
+var4 = "44"
+var5 = "55"
+print (int (var4) + int (var5))
+print(10 * "AHMAD \n" )
+print (10 * str(int (var4) + int (var5)))
+#input
+"""print("Enter number")
+inpnum = input()
+print("You entered ", int(inpnum) + 10)
+#Practice
+print("Enter first number")
+inp = input()
+print("Enter second number")
+inp2 = input()
+print("The sum is", int(inp) + int(inp2))
+"""
+
+# Slicing
+
+str = "my name is AHMAD"
+print(str)
+print(len(str))
+print(str[0:16:1])
+print((str[ ::-1])) #Reverse the string
+
+#different Functions
+
+print(str.endswith("D"))
+print(str.capitalize())
+print(str.find("is"))
+print(str.lower())
+print(str.upper())
+print(str.replace("AHMAD", "AHMAD KHALID"))
+
+
