46e22469d171d5acf3d28ce04c1be824d6bf6e90
Ahmad Khalid <danialkhalid63@gmail.com>
2021-11-23 17:09:20 +0500

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,33 +1,60 @@
-# gros = ['bread', "milk", "cookies"]
-# print (gros)
+# # gros = ['bread', "milk", "cookies"]
+# # print (gros)
+# #
+# # # For Loop
+# #
+# # exp = [2340, 2500, 2100, 3100, 2980]
+# # total = 0 ;
+# #
+# # for i in exp:
+# #
+# #         total = total+i
+# # print(total)
 #
-# # For Loop
 #
-# exp = [2340, 2500, 2100, 3100, 2980]
-# total = 0 ;
 #
-# for i in exp:
+# # 2
 #
-#         total = total+i
-# print(total)
+# # for i in range(1,11):
+# #     print(i)
+#
+#
+# # 3
+#
+# key_location = "chair"
+# locations = ["bed Room", "Lounge", "garage", "chair", "Kitchen"]
+#
+# for i in locations:
+#     if i == key_location:
+#         print("Key is found in ", i)
+#         break
+#
+#     else:
+#         print("Key is not found in ", i)
+#
+#
+#
+#
+#
+# # Functionsq
+#
 
 
+def Calculate_Total(exp):
+    total = 0;
+    for i in exp:
+        total = total + i;
 
-# 2
-
-# for i in range(1,11):
-#     print(i)
+    return total;
 
 
-# 3
+Ali_exp = [700, 500, 200]
+Ahmad_exp = [2100, 3400, 3500]
 
-key_location = "chair"
-locations = ["bed Room", "Lounge", "garage", "chair", "Kitchen"]
+# To calculate the total of both of them individually
 
-for i in locations:
-    if i == key_location:
-        print("Key is found in ", i)
-        break
+Ali_Total = Calculate_Total(Ali_exp)
+Ahmad_Total = Calculate_Total(Ahmad_exp)
+print("Ali Expenses is ", Ali_Total)
+print("Ahmad Expenses is ", Ahmad_Total)
 
-    else:
-        print("Key is not found in ", i)
