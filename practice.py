578174f47c84f15e2d8b059ae5ff27a1fb21420f
Ahmad Khalid <danialkhalid63@gmail.com>
2021-12-02 00:08:44 +0500

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,81 +1,116 @@
-# # # gros = ['bread', "milk", "cookies"]
-# # # print (gros)
+# # # # gros = ['bread', "milk", "cookies"]
+# # # # print (gros)
+# # # #
+# # # # # For Loop
+# # # #
+# # # # exp = [2340, 2500, 2100, 3100, 2980]
+# # # # total = 0 ;
+# # # #
+# # # # for i in exp:
+# # # #
+# # # #         total = total+i
+# # # # print(total)
 # # #
-# # # # For Loop
 # # #
-# # # exp = [2340, 2500, 2100, 3100, 2980]
-# # # total = 0 ;
 # # #
-# # # for i in exp:
+# # # # 2
 # # #
-# # #         total = total+i
-# # # print(total)
+# # # # for i in range(1,11):
+# # # #     print(i)
+# # #
+# # #
+# # # # 3
+# # #
+# # # key_location = "chair"
+# # # locations = ["bed Room", "Lounge", "garage", "chair", "Kitchen"]
+# # #
+# # # for i in locations:
+# # #     if i == key_location:
+# # #         print("Key is found in ", i)
+# # #         break
+# # #
+# # #     else:
+# # #         print("Key is not found in ", i)
+# # #
+# # #
+# # #
+# # #
+# # #
+# # # # Functionsq
+# # #
 # #
 # #
+# # def Calculate_Total(exp):
+# #     total = 0;
+# #     for i in exp:
+# #         total = total + i;
 # #
-# # # 2
-# #
-# # # for i in range(1,11):
-# # #     print(i)
-# #
-# #
-# # # 3
-# #
-# # key_location = "chair"
-# # locations = ["bed Room", "Lounge", "garage", "chair", "Kitchen"]
-# #
-# # for i in locations:
-# #     if i == key_location:
-# #         print("Key is found in ", i)
-# #         break
-# #
-# #     else:
-# #         print("Key is not found in ", i)
+# #     return total;
 # #
 # #
+# # Ali_exp = [700, 500, 200]
+# # Ahmad_exp = [2100, 3400, 3500]
 # #
+# # # To calculate the total of both of them individually
 # #
-# #
-# # # Functionsq
+# # Ali_Total = Calculate_Total(Ali_exp)
+# # Ahmad_Total = Calculate_Total(Ahmad_exp)
+# # print("Ali Expenses is ", Ali_Total)
+# # print("Ahmad Expenses is ", Ahmad_Total)
 # #
 #
 #
-# def Calculate_Total(exp):
-#     total = 0;
-#     for i in exp:
-#         total = total + i;
+# # Dictionary
 #
-#     return total;
+# d = {"Ali": 123456, "asad":4567890, "Enujm" : 3989897};
+# # print (d['asad'])
 #
 #
-# Ali_exp = [700, 500, 200]
-# Ahmad_exp = [2100, 3400, 3500]
+# # To add into dictionary
 #
-# # To calculate the total of both of them individually
+# d["ahmad"]=1098765;
+# print(d)
 #
-# Ali_Total = Calculate_Total(Ali_exp)
-# Ahmad_Total = Calculate_Total(Ahmad_exp)
-# print("Ali Expenses is ", Ali_Total)
-# print("Ahmad Expenses is ", Ahmad_Total)
+# # To delete element into dictionary
+# del d["Enujm"]
+# print(d)
 #
-
-
-# Dictionary
-
-d = {"Ali": 123456, "asad":4567890, "Enujm" : 3989897};
-# print (d['asad'])
-
-
-# To add into dictionary
-
-d["ahmad"]=1098765;
-print(d)
-
-# To delete element into dictionary
-del d["Enujm"]
-print(d)
+#
+#
+# for key in d:
+#     print("Key:" , key, "value: ", d[key])
 
 
 
-for key in d:
-    print("Key:" , key, "value: ", d[key])
+
+#JSON Format
+
+book={}
+
+book['Ali'] = {
+    'Name': 'Ali',
+    'address':'Raiwind road lahore',
+    'phone': 1234556666
+}
+
+book["Usman"] = {
+    'Name': "Usman",
+    'address':'Johar Town',
+    'phone': 1867675656687
+}
+
+book["Ahmad"] = {
+    'Name': "Ahmad",
+    'address':'Model Town',
+    'phone': 536484778788
+}
+
+
+import json
+s= json.dumps( book)
+# print(s)
+
+with open("D:\DS\project", "w") as ba:
+    ba.write(s)
+
+
